4bf67f47014a07d30dc7886e1025c8aaa7a73939
Titus Riemersma <titus@tangibleapp.nl>
2020-08-08 10:30:59 +0200

    Better solution to create svg within container

--- src/D3Component.fs
+++ src/D3Component.fs
@@ -51,14 +51,11 @@ let D3Component containerId (props: LocalModel) =
 // https://fable.io/docs/communicate/js-from-fable.html#Other-special-attributes
 // https://fsharpforfunandprofit.com/posts/currying/
 let D3Barchart containerId =
-  let d3container : D3.Selection.Selection<obj,obj,Browser.Types.HTMLElement,obj option> =
-        D3.d3.select(sprintf "#%s" containerId)
+  // let d3container : D3.Selection.Selection<obj,obj,Browser.Types.HTMLElement,obj option> =
+  //       D3.d3.select(sprintf "#%s" containerId)
   // NOTE: svg conainer defaults to 300x150px when calculation failes: https://www.w3.org/TR/CSS21/visudet.html#inline-replaced-width
-
-  d3container.select("svg").remove() |> ignore// little hack to make sure we'll have only 1 svg container
-  // TODO: need nicer solution that creates svg container only once, more in line with react component lifecyle
   let svg =
-    d3container
+    D3.d3.select(sprintf "#%s > svg" containerId)
       .append("svg")
       .attr("width", 500)
       .attr("height", 100)
@@ -90,7 +87,9 @@ let D3Barchart containerId =
       .attr("x", System.Func<_,_,_>(fun _ i -> i * (width/dataSetLength))) // To access second, iterator, parameter, we need currying: https://fsharpforfunandprofit.com/posts/currying/
       .attr("y", fun data -> height - (float data * 4.))
   |> ignore
-  Container.container [ Container.Props [Id containerId ]] []
+  Container.container [ Container.Props [Id containerId ]] [
+     Fable.React.Standard.svg [][]
+  ]
 
 // Fable D3 Barchart sample remake in REPL
 // https://fable.io/repl/#?code=FAehAICUFMGMCdoEMAuBLA9gO3BgZuAGJIBGANtOAEJLywAWtK4AykgLYAOF49KKnAM4AuMHlIUAdJhDjy0ALQBzeEk700sQSEEdu0bQBMAzCBK0GTEGiyHoAD0l92ZUBHODoh3DkgBRAAUAGXA8eAx2Xn4hURAlNBR6AFcSSVgI2QlFdK40CngQYnlrQUEkgxAARgAOACZa4GAMTmgcIqkAYQxEJpa2rMkuxEkAKUEASSwUaHDORopmAG0AHgBxMgxzMgA+AF1wE2FcEgArcABecBPBADlUNAA3aEaUAE8W1lfBafZJAHlTnBmAB3BL0YDgSHsaDsEgzcCJNCCSTYAAU0CeUzu0KO33gNiUABpwLAkGQyOZYABrI6bE4KbZJLAJACUtNOFwhkO5kMRggA-GiMa0UNjoMS8EzwKiWeAGSSyRSkNSroIACoaQQs+bQZh4PJkcDfV48PmcnmGDAHYz8zwUWAoVF8lm2t4UVEAIn15I9xONFG1wAWRr0FBYADVVuyzpcuZCTLboPbHR6AMQPNAALw92p5PP5aj6hk9ggeShzcbzBf48E9oMMiV94AArAAGVu5vOQ6soWse+jQNBKPhNgAs7cDwdUtgiF3AWGgwM+3xhkkgSBn7BlQd1B1QSE8zEuC6XMEEWegAEF4KpXssbChttLr7fpMzmLVm9LJTgAPpyp9UWnS1fhuBxHWMQlPxZGDtWDcx4AACUHYdmHsOd0IAKhbcAd2YBCAiQQxDAJOdKkkPDwHrRI5zbVsKODAchz4Mj2wY3dDH3FhdSCVolBoy48A2VA9xQA9dUGDAmRQRpLRDLgw0jStE2TS9yU9RAHQrfNOLE1FdPElBZTAUSkHnaAvEEBErThKADAvF8kFeZSRRmGVwBM1z4CNJMgUwLBlMLVpiw9TSUBzDyIASGZUEoRJwiSYdwF86EpkEQKazrNAG3oJsWC+H5JAAWVQehJEvEhBFRaj6A80zuJQXisH42qFHAAiiJI5qYIy3tPSY1Cmx-UyAJG7DR0kTtu1QPqPXsPKCtXQgmVgZZf0Jdbf22VFhv-NBRv27DquyxIQAMhqmpa2DIvANUrWVWADCszx0lsYlotUFBumJThaA4XUZmJYFKAXLwSSSG9XgJI4+AEEQxEERh4E4PBuh-DdDE4cJ9RQNIMk4DBvm0WAIfgKHmpAXq+1eIapQM0aBpYtqhIwET6fGyaqc9b0yFpnB6flQQsYfAhQqUEhUQANkggBSS9CUqNsWQ9aV2fASoO21AAfJ8hywbpni7LtgDk3QFOgCNVmUu0gTUshPWmexwqm8B+QM-T90PF3+S87d8yC2wHfA7TuX5R3HWGgWnwM72Zr7ebiXyldfmWrBVs2jbtt28B9vlQ7pRqs6uJ4vjEmuky7vAB6np817DHe6ZPu+8BftUaFG6BkGLO8EnIehqI4diPBEdoFG0aZDGsfwBI8fYEACaJkBe7JglKf9zKPRpiU6f3BmUKZ79hOYNWJp6nWc6UfXECAA&html=DwQgIg8gwgKgmgBQKIAIAWAXAtgGwHwBQwmuhxApgIYAmhK9KwAzgMYBOAlgA4YpNssAvACJMGLkwBcAemnUAzACsmAOgD2bAOZz5KgG4BWFVg4A7FcuF5g01px5lpaKrSIAjNdQCedBsGoceigc1CJ6HABeVjYBeo4e3o4k+EA&css=Q
